1c75167040f1ba940d4863086c1f2474c768e968
Mathias Hillesheim <mathias.hillesheim@byne.com.br>
2017-03-19 16:18:41 -0300

    Main struct changes inside protocol

--- ControlProtocol.py
+++ ControlProtocol.py
@@ -1,113 +1,114 @@
 from control_models import cr2fp
 from binascii import unhexlify
 
+from twisted.internet.protocol import DatagramProtocol
+from twisted.internet import reactor
 
-class ControlProtocol(object):
+
+class ControlProtocol(DatagramProtocol):
 
     def __init__(self, model):
         if model == 'cr2fp':
-            self.control = cr2fp.CR2FP()
-            print('omg omg omg: %r' % self.control)
+            self.control = cr2fp
         else:
             raise
+        self.host = "192.168.1.238"
+        self.port = 7001
+        self.transport.connect(self.host, self.port)
 
-    # BINARY SECTION:
-    def volume_down(self):   # return the binary code for VOLUME DOWN
+    def protocol_callback(self):
+        # callback used only for tests with a computer keyboard
+        while True:
+            button_pressed = raw_input()
+            if button_pressed == '0':
+                self.number0
+            elif button_pressed == '1':
+                self.number1
+            elif button_pressed == '2':
+                self.number2
+            elif button_pressed == '3':
+                self.number3
+            elif button_pressed == '4':
+                self.number4
+            elif button_pressed == '5':
+                self.number5
+            elif button_pressed == '6':
+                self.number6
+            elif button_pressed == '7':
+                self.number7
+            elif button_pressed == '8':
+                self.number8
+            elif button_pressed == '9':
+                self.number9
+            elif button_pressed == 'h':
+                self.volume_down
+            elif button_pressed == 'l':
+                self.volume_up
+            elif button_pressed == 'j':
+                self.channel_down
+            elif button_pressed == 'k':
+                self.channel_up
+            else:
+                print("Wrong input")
+
+    def _send(self, command):
         try:
-            cmd = unhexlify(self.control.VOLUME_DOWN)
+            self.transport.write(unhexlify(command))
         except Exception as error:
-            cmd = None
-        return cmd
+            print(error)
+
+    def volume_down(self):   # return the binary code for VOLUME DOWN
+        cmd = self.control.VOLUME_DOWN
+        self._send(cmd)
 
     def volume_up(self):     # return the binary code for VOLUME UP
-        try:
-            cmd = unhexlify(self.control.VOLUME_UP)
-        except Exception as error:
-            cmd = None
-        return cmd
+        cmd = self.control.VOLUME_UP
+        self._send(cmd)
 
     def channel_down(self):  # return the binary code for CHANNEL DOWN
-        try:
-            cmd = unhexlify(self.control.CHANNEL_DOWN)
-        except Exception as error:
-            cmd = None
-        return cmd
+        cmd = self.control.CHANNEL_DOWN
+        self._send(cmd)
 
     def channel_up(self):    # return the binary code for CHANNEL_UP
-        try:
-            cmd = unhexlify(self.control.CHANNEL_UP)
-        except Exception as error:
-            cmd = None
-        return cmd
+        cmd = self.control.CHANNEL_UP
+        self._send(cmd)
 
     def number0(self):       # return the binary code for NUMBER 0
-        try:
-            cmd = unhexlify(self.control.NUMBER_0)
-        except Exception as error:
-            cmd = None
-        return cmd
+        cmd = self.control.NUMBER_0
+        self._send(cmd)
 
     def number1(self):       # return the binary code for NUMBER 1
-        try:
-            cmd = unhexlify(self.control.NUMBER_1)
-            print("entrou no try")
-        except Exception as error:
-            cmd = None
-            print("entrou no except")
-        return cmd
+        cmd = self.control.NUMBER_1
+        self._send(cmd)
 
     def number2(self):       # return the binary code for NUMBER 2
-        try:
-            cmd = unhexlify(self.control.NUMBER_2)
-        except Exception as error:
-            cmd = None
-        return cmd
+        cmd = self.control.NUMBER_2
+        self._send(cmd)
 
     def number3(self):       # return the binary code for NUMBER 3
-        try:
-            cmd = unhexlify(self.control.NUMBER_3)
-        except Exception as error:
-            cmd = None
-        return cmd
+        cmd = self.control.NUMBER_3
+        self._send(cmd)
 
     def number4(self):       # return the binary code for NUMBER 4
-        try:
-            cmd = unhexlify(self.control.NUMBER_4)
-        except Exception as error:
-            cmd = None
-        return cmd
+        cmd = self.control.NUMBER_4
+        self._send(cmd)
 
     def number5(self):       # return the binary code for NUMBER 5
-        try:
-            cmd = unhexlify(self.control.NUMBER_5)
-        except Exception as error:
-            cmd = None
-        return cmd
+        cmd = self.control.NUMBER_5
+        self._send(cmd)
 
     def number6(self):       # return the binary code for NUMBER 6
-        try:
-            cmd = unhexlify(self.control.NUMBER_6)
-        except Exception as error:
-            cmd = None
-        return cmd
+        cmd = self.control.NUMBER_6
+        self._send(cmd)
 
     def number7(self):       # return the binary code for NUMBER 7
-        try:
-            cmd = unhexlify(self.control.NUMBER_7)
-        except Exception as error:
-            cmd = None
-        return cmd
+        cmd = self.control.NUMBER_7
+        self._send(cmd)
 
     def number8(self):       # return the binary code for NUMBER 8
-        try:
-            cmd = unhexlify(self.control.NUMBER_8)
-        except Exception as error:
-            cmd = None
-        return cmd
+        cmd = self.control.NUMBER_8
+        self._send(cmd)
 
     def number9(self):       # return the binary code for NUMBER 9
-        try:
-            cmd = unhexlify(self.control.NUMBER_9)
-        except Exception as error:
-            cmd = None
-        return cmd
+        cmd = self.control.NUMBER_9
+        self._send(cmd)
