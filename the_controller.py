3c0b9b5d130a780a46cb0315117fb6639ff917e0
Mathias Hillesheim <mathias.hillesheim@byne.com.br>
2017-03-22 17:16:50 -0300

    Changed the comments from hash to docstring

--- the_controller.py
+++ the_controller.py
@@ -7,39 +7,48 @@ from twisted.internet.protocol import DatagramProtocol
 
 class ControlProtocol(DatagramProtocol):
 
-    # remote control models available
-    # it needs a better approach if the number of models grows
     def __init__(self, model):
+        """
+            Remote control models that are available.
+            It needs a better approach if the number of models grows.
+        """
         if model == 'cr2fp':
             self.control = cr2fp.CR2FP()
         elif model == 'skyhdtv':
             self.control = skyhdtv.SKYHDTV()
         else:
             raise
-        # the HDbitT device works with a default IP (192.168.1.238)
-        # the default port for the IR stream is 7001
+        """
+            The HDbitT device works with a default IP (192.168.1.238)
+            The default port for the IR stream is 7001
+        """
         self.host = "192.168.1.238"
         self.port = 7001
 
-    # default reactor method startProtocol, used here to create the 
-    # socket connection and call the main protocol loop
     def startProtocol(self):
+        """
+            Default reactor method startProtocol, used here to create the
+            socket connection and call the main protocol loop
+        """
         self.transport.connect(self.host, self.port)
         print("Connect with %s:%d" % (self.host, self.port))
         self.protocol_loop()
 
-    # default reactor method (useless for the protocol itself)
     def datagramReceived(self, data, addr):
+        """Default reactor method (useless for the protocol itself)"""
         print("Received %r from %s." % (data, addr))
 
-    # default reactor method (useless for the protocol itself)
     def connectionRefused(self):
+        """Default reactor method (useless for the protocol itself)"""
         print("Connection refused: no one listening.")
 
-    # 'input_validation' method returns a list of validated parsed characters
-    # or None if the chars are not valid. This way the user can insert a string
-    # of buttons and the program will work just fine
     def input_validation(self, keyboard_input):
+        """
+            input_validation method returns a list of validated parsed
+            characters if all the characters are valid, or None if the
+            characters are not valid. This way the user can insert a string of
+            buttons and the program will work just fine.
+        """
         list_of_buttons = []
         for letter in keyboard_input:
             if letter in self.control.buttons:
@@ -48,9 +57,11 @@ class ControlProtocol(DatagramProtocol):
                 return None
         return list_of_buttons
 
-    # main program loop, used to read the input from the user and make the
-    # general decisions
     def protocol_loop(self):
+        """
+            Main program loop, used to read the input from the user and make
+            the general method calls.
+        """
         while True:
             keyboard_input = None
             keyboard_input = raw_input()
@@ -62,9 +73,11 @@ class ControlProtocol(DatagramProtocol):
                     sleep(self.control.message_delay)
                     self._send(self.control.buttons[list_of_cmd.pop(0)])
 
-    # '_send' method receive the command string, convert it to binary and
-    # send it to the socket
     def _send(self, command):
+        """
+            _send method receive the command string, convert it to binary and
+            send it to the socket.
+        """
         try:
             self.transport.write(unhexlify(command))
         except Exception as error:
