a17369c7297f66321ad10e944f8a5c75eff6e32a
Mathias Hillesheim <mathiashls@protonmail.com>
2017-03-23 18:16:26 -0300

    Included logging, improved commenting and modularization.

--- the_controller.py
+++ the_controller.py
@@ -1,4 +1,5 @@
 from binascii import unhexlify
+import logging
 from time import sleep
 from control_models import cr2fp
 from control_models import skyhdtv
@@ -7,7 +8,7 @@ from twisted.internet.protocol import DatagramProtocol
 
 class ControlProtocol(DatagramProtocol):
 
-    def __init__(self, model):
+    def __init__(self, model, host, port):
         """
             Remote control models that are available.
             It needs a better approach if the number of models grows.
@@ -18,12 +19,8 @@ class ControlProtocol(DatagramProtocol):
             self.control = skyhdtv.SKYHDTV()
         else:
             raise
-        """
-            The HDbitT device works with a default IP (192.168.1.238)
-            The default port for the IR stream is 7001
-        """
-        self.host = "192.168.1.238"
-        self.port = 7001
+        self.host = host
+        self.port = port
 
     def startProtocol(self):
         """
@@ -31,16 +28,16 @@ class ControlProtocol(DatagramProtocol):
             socket connection and call the main protocol loop
         """
         self.transport.connect(self.host, self.port)
-        print("Connect with %s:%d" % (self.host, self.port))
+        logging.info("Connect with %s:%d" % (self.host, self.port))
         self.protocol_loop()
 
     def datagramReceived(self, data, addr):
         """Default reactor method (useless for the protocol itself)"""
-        print("Received %r from %s." % (data, addr))
+        logging.info("Received %r from %s." % (data, addr))
 
     def connectionRefused(self):
         """Default reactor method (useless for the protocol itself)"""
-        print("Connection refused: no one listening.")
+        logging.error("Connection refused: no one listening.")
 
     def input_validation(self, keyboard_input):
         """
@@ -50,28 +47,36 @@ class ControlProtocol(DatagramProtocol):
             buttons and the program will work just fine.
         """
         list_of_buttons = []
-        for letter in keyboard_input:
-            if letter in self.control.buttons:
-                list_of_buttons.append(letter)
-            else:
-                return None
+        if keyboard_input in self.control.buttons:
+            list_of_buttons.append(keyboard_input)
+        else:
+            for letter in keyboard_input:
+                if letter in self.control.buttons:
+                    list_of_buttons.append(letter)
+                else:
+                    return None
         return list_of_buttons
 
     def protocol_loop(self):
         """
+            protocol_loop must be overwritten by the application. It must
+            capture the input from the user and send it to the
+            protocol_dispatcher
+        """
+        raise
+
+    def protocol_dispatcher(self, keyboard_input):
+        """
             Main program loop, used to read the input from the user and make
             the general method calls.
         """
-        while True:
-            keyboard_input = None
-            keyboard_input = raw_input()
-            list_of_cmd = self.input_validation(keyboard_input)
-            if list_of_cmd is None:
-                print("Wrong input parameters: try again.")
-            else:
-                while list_of_cmd:
-                    sleep(self.control.message_delay)
-                    self._send(self.control.buttons[list_of_cmd.pop(0)])
+        list_of_cmd = self.input_validation(keyboard_input)
+        if list_of_cmd is None:
+            logging.warn("Wrong input parameters: try again.")
+        else:
+            while list_of_cmd:
+                sleep(self.control.INTERVAL)
+                self._send(self.control.buttons[list_of_cmd.pop(0)])
 
     def _send(self, command):
         """
@@ -81,4 +86,4 @@ class ControlProtocol(DatagramProtocol):
         try:
             self.transport.write(unhexlify(command))
         except Exception as error:
-            print(error)
+            logging.error(error)
